b7c9459d65e0b5be245ec5cdf7edf3394926da3a
James Brainard <james.brainard2@aol.com>
2023-09-13 20:54:44 -0700

    modified index js file to make functional, generatemarkdown file has been created with functional code

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# professional-readme-generator
-An easy to use application that will prompt you for answers to generate a readme.
+
+  [![License](https://img.shields.io/badge/License-MIT-blue)](https://opensource.org/licenses/MIT)
+  # Professional Readme Generator
+
+  ## Description
+  This application will assist you in creating a professional readme.md with ease.
+
+  ## Table of Contents
+  * [Installation](#install)
+  * [Usage](#usage)
+  * [Contributions](#contribute)
+  * [Test](#test)
+  * [License](#license)
+  
+  ## Installation
+  n/a
+
+  ## Usage 
+  n/a
+
+  ## Contributions
+  n/a
+
+  ## Test
+  n/a
+
+  ## License
+  MIT
---
+++ develop/utils/generateMarkdown.js
@@ -0,0 +1,80 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license === undefined) {
+    return ''
+  } else {
+    return `[![License](https://img.shields.io/badge/License-${license}-blue)](${renderLicenseLink(license)})`
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// switch below
+function renderLicenseLink(license) {
+  switch (license) {
+    case "Apache":
+      return 'https://opensource.org/licenses/Apache-2.0'
+      break;
+  
+    case "GNU":
+      return 'https://www.gnu.org/licenses/gpl-3.0'
+      break;
+  
+    case "MIT":
+      return 'https://opensource.org/licenses/MIT'
+      break;
+  
+    case "GPLv3":
+      return 'https://www.gnu.org/licenses/gpl-3.0'
+      break;
+  }
+
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license === undefined) {
+    return ''
+  } else {
+    return `This project is covered under ${license}`
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+  ${renderLicenseBadge(data.license)}
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#install)
+  * [Usage](#usage)
+  * [Contributions](#contribute)
+  * [Test](#test)
+  * [License](#license)
+  
+  ## Installation
+  ${data.install}
+
+  ## Usage 
+  ${data.usage}
+
+  ## Contributions
+  ${data.contribute}
+
+  ## Test
+  ${data.test}
+
+  ## License
+  ${data.license}
+`;
+}
+
+module.exports = generateMarkdown
+
+// create badges and license objects here?
